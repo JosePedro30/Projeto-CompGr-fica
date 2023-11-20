3e17cebe100d8ffafea6469c34d988336715282d
Diogo Lopes <40220401@esmad.ipp.pt>
2023-11-20 14:49:25 +0000

    .

--- script.js
+++ script.js
@@ -9,86 +9,102 @@ ctx.lineWidth = 0,2;
 var wiggleAmplitude = 5;
 
 
-diam = 6
+diam = 10
 
 function Onda() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    
-    ctx.moveTo(0, canvas.height / 2); //mover para a posição central no eixo do y
+    ctx.moveTo(0, canvas.height / 2); //mover para a posição central
 
     amplitude += 0.5 * wiggleAmplitude - wiggleAmplitude / 2;
 
-    for(var x = 0; x < canvas.width; x++) {
+    for(var x = 0; x < canvas.width - (canvas.width / 2); x++) {
         
         var y = amplitude * Math.sin(frequency * x + phase);
-        console.log(y)
 
         if(y>0){ //metade de baixo
-            ctx.fillStyle = "red";
+            ctx.strokeStyle = "red";
+            ctx.fillStyle = "black";
             if(y>0 && y<10){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.beginPath();
                 ctx.arc(x, y + canvas.height / 2, diam, 0, 2 * Math.PI);
                 ctx.closePath();
                 ctx.fill();
+                ctx.stroke();
             }
             if(y>10 && y<20){
-                ctx.beginPath();
-                ctx.arc(x, y + canvas.height / 2, diam-0.5, 0, 2 * Math.PI);
-                ctx.closePath();
-                ctx.fill();
-            }
-            if(y>20 && y<30){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.beginPath();
                 ctx.arc(x, y + canvas.height / 2, diam-1, 0, 2 * Math.PI);
                 ctx.closePath();
                 ctx.fill();
+                ctx.stroke();
             }
-            if(y>30 && y<40){
-                ctx.beginPath();
-                ctx.arc(x, y + canvas.height / 2, diam-1.5, 0, 2 * Math.PI);
-                ctx.closePath();
-                ctx.fill();
-            }
-            if(y>40 && y<50){
+            if(y>20 && y<30){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.beginPath();
                 ctx.arc(x, y + canvas.height / 2, diam-2, 0, 2 * Math.PI);
                 ctx.closePath();
                 ctx.fill();
+                ctx.stroke();
+            }
+            if(y>30 && y<40){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.beginPath();
+                ctx.arc(x, y + canvas.height / 2, diam-3, 0, 2 * Math.PI);
+                ctx.closePath();
+                ctx.fill();
+                ctx.stroke();
+            }
+            if(y>40 && y<50){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.beginPath();
+                ctx.arc(x, y + canvas.height / 2, diam-4, 0, 2 * Math.PI);
+                ctx.closePath();
+                ctx.fill();
+                ctx.stroke();
             }
         }
 
         if(y<0){ //metade do topo
-            ctx.fillStyle = "blue";
             if(y<0 && y>-10){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.beginPath();
                 ctx.arc(x, y + canvas.height / 2, diam, 0, 2 * Math.PI);
                 ctx.closePath();
                 ctx.fill();
+                ctx.stroke();
             }
             if(y<-10 && y>-20){
-                ctx.beginPath();
-                ctx.arc(x, y + canvas.height / 2, diam-0.5, 0, 2 * Math.PI);
-                ctx.closePath();
-                ctx.fill();
-            }
-            if(y<-20 && y>-30){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.beginPath();
                 ctx.arc(x, y + canvas.height / 2, diam-1, 0, 2 * Math.PI);
                 ctx.closePath();
                 ctx.fill();
+                ctx.stroke();
             }
-            if(y<-30 && y>-40){
-                ctx.beginPath();
-                ctx.arc(x, y + canvas.height / 2, diam-1.5, 0, 2 * Math.PI);
-                ctx.closePath();
-                ctx.fill();
-            }
-            if(y<-40 && y>-50){
+            if(y<-20 && y>-30){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.beginPath();
                 ctx.arc(x, y + canvas.height / 2, diam-2, 0, 2 * Math.PI);
                 ctx.closePath();
                 ctx.fill();
+                ctx.stroke();
+            }
+            if(y<-30 && y>-40){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.beginPath();
+                ctx.arc(x, y + canvas.height / 2, diam-3, 0, 2 * Math.PI);
+                ctx.closePath();
+                ctx.fill();
+                ctx.stroke();
+            }
+            if(y<-40 && y>-50){
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.beginPath();
+                ctx.arc(x, y + canvas.height / 2, diam-4, 0, 2 * Math.PI);
+                ctx.closePath();
+                ctx.fill();
+                ctx.stroke();
             }
 
         }
