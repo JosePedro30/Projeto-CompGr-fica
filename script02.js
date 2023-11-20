7a4bf6d7eccaf8437b705d6c4d38964d1e70b106
DiogoLopess <40220401@esmad.ipp.pt>
2023-11-20 19:09:48 +0000

    Final

--- script02.js
+++ script02.js
@@ -3,21 +3,40 @@ const ctx = canvas.getContext("2d");
 
 const circle = {
     x: 0,
-    y: canvas.height / 2,
-    amplitude: 50,
+    y: 0,
+    amplitude: 2,
     angle: 0,
     phase: 0,
-    diam: 10,
+    diam: 0.4,
 };
 
-function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+const circle2 = {
+    x: 0,
+    y: 100,
+    amplitude: circle.amplitude + 1,
+    angle: 0,
+    phase: 0,
+    diam: 0.4,
+};
 
-    circle.x++;  // Movendo o círculo para a direita
+const circle3 = {
+    x: 0,
+    y: 100,
+    amplitude: circle2.amplitude + 2,
+    angle: 0,
+    phase: 0,
+    diam: 0.4,
+};
+
+function Circ1() {
+    ctx.fillStyle = "blue";
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    circle.x = circle.x + 0.3;  // Movendo o círculo para a direita
 
     // Atualizando a posição y do círculo com uma onda senoidal
-    circle.y = canvas.height / 2 + circle.amplitude * Math.sin(circle.angle + circle.phase);
-    circle.angle += 0.1;
+    circle.y = 135 + circle.amplitude * Math.sin(circle.angle + circle.phase);
+    circle.angle += 0.05;
 
     // Desenhando o círculo
     ctx.beginPath();
@@ -25,13 +44,78 @@ function render() {
     ctx.fill();
     ctx.closePath();
 
-    // Verificando se o círculo ultrapassou o limite direito do canvas
+    //Quadrado
+    ctx.beginPath();
+    ctx.rect(circle.x - (circle.diam), circle.y, circle.diam * 2, canvas.height);
+    ctx.fill();
+
+    // Verificar se o círculo ultrapassou o limite direito do canvas
     if (circle.x > canvas.width + circle.diam) {
-        circle.x = -circle.diam;  // Movendo o círculo de volta para o início
+        circle.x = 0;  // Mover o círculo de volta para o início
     }
 
-    requestAnimationFrame(render);
+    requestAnimationFrame(Circ1);
+}
+
+function Circ2() {
+    ctx.fillStyle = "red";
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    circle2.x = circle.x + 0.3;  // Movendo o círculo para a direita
+
+    // Atualizando a posição y do círculo com uma onda senoidal
+    circle2.y = circle.y - 20 + circle2.amplitude * Math.sin(circle2.angle + circle2.phase);
+    circle2.angle += 0.05;
+
+    // Desenhando o círculo
+    ctx.beginPath();
+    ctx.arc(circle2.x, circle2.y, circle2.diam, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+
+    //Quadrado
+    ctx.beginPath();
+    ctx.rect(circle2.x - (circle2.diam), circle2.y, circle2.diam * 2, circle.y - circle2.y);
+    ctx.fill();
+
+    // Verificar se o círculo ultrapassou o limite direito do canvas
+    if (circle2.x > canvas.width + circle2.diam) {
+        circle2.x = 0;  // Mover o círculo de volta para o início
+    }
+
+    requestAnimationFrame(Circ2);
+}
+
+function Circ3() {
+    ctx.fillStyle = "green";
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    circle3.x = circle2.x + 0.3;  // Movendo o círculo para a direita
+
+    // Atualizando a posição y do círculo com uma onda senoidal
+    circle3.y = circle2.y - 20 + circle3.amplitude * Math.sin(circle3.angle + circle3.phase);
+    circle3.angle += 0.05;
+
+    // Desenhando o círculo
+    ctx.beginPath();
+    ctx.arc(circle3.x, circle3.y, circle3.diam, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+
+    //Quadrado
+    ctx.beginPath();
+    ctx.rect(circle3.x - (circle3.diam), circle3.y, circle3.diam * 2, circle2.y - circle3.y);
+    ctx.fill();
+
+    // Verificar se o círculo ultrapassou o limite direito do canvas
+    if (circle3.x > canvas.width + circle3.diam) {
+        circle3.x = 0;  // Mover o círculo de volta para o início
+    }
+
+    requestAnimationFrame(Circ3);
 }
 
 // Iniciando a animação
-render();
+Circ1();
+Circ2();
+Circ3();
