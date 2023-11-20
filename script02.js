282d0b131a4908edd1c736882d61b0912a176492
JosePedro30 <josepedropgs@gmail.com>
2023-11-20 15:55:08 +0000

    Fiz a animação que faz o círculo recomeçar animaçã

--- script02.js
+++ script02.js
@@ -1,48 +1,37 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-var amplitude = 50;
-var frequency = 0.05;
-var phase = 0;
-ctx.lineWidth = 0.2;
-
-var wiggleAmplitude = 5;
-
-
-let circle = {
-    amplitude: 20, angle: 0, x: 0, y:0, diam: 2, phase: 0
-}
-
-let circle2 = {
-    amplitude: 30, angle: 0, x: 0, y:0, diam: 5, phase: Math.PI/2
-}
+const circle = {
+    x: 0,
+    y: canvas.height / 2,
+    amplitude: 50,
+    angle: 0,
+    phase: 0,
+    diam: 10,
+};
 
 function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    circle.x++
-    circle.y = canvas.height/2 + circle.amplitude * Math.sin(circle.angle + circle.phase );
-    circle.angle += 0.1
+    circle.x++;  // Movendo o círculo para a direita
 
-    ctx.beginPath()
-    ctx.arc(circle.x, circle.y , circle.diam, 0, 2 * Math.PI);
-    ctx.fill()
+    // Atualizando a posição y do círculo com uma onda senoidal
+    circle.y = canvas.height / 2 + circle.amplitude * Math.sin(circle.angle + circle.phase);
+    circle.angle += 0.1;
 
+    // Desenhando o círculo
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.diam, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
 
-    // circle2.x++
-    // circle2.y = canvas.height/2 + circle2.amplitude * Math.sin(circle2.angle  + circle2.phase);
-    // circle2.angle += 0.1
-
-    // ctx.beginPath()
-    // ctx.arc(circle2.x, circle2.y , circle2.diam, 0, 2 * Math.PI);
-    // ctx.fill()
-
-
-    
+    // Verificando se o círculo ultrapassou o limite direito do canvas
+    if (circle.x > canvas.width + circle.diam) {
+        circle.x = -circle.diam;  // Movendo o círculo de volta para o início
+    }
 
     requestAnimationFrame(render);
-
 }
 
-render()
-
+// Iniciando a animação
+render();
